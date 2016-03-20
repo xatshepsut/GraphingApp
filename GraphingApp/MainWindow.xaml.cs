756b5ceb6ba2ba9fbc12b0197e5f4e4a6aa955dd
Aidela Karamyan <akaramyan@macadamian.com>
2016-03-20 22:56:35 +0400

    Implements visual node add/remove logic

--- GraphingApp/MainWindow.xaml.cs
+++ GraphingApp/MainWindow.xaml.cs
@@ -1,27 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace GraphingApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void addNode(double top, double left)
+        {
+            var node= new Node();
+            node.RemoveTriggered += this.node_RemoveTriggered;
+
+            Canvas.SetTop(node, top - node.Diameter / 2);
+            Canvas.SetLeft(node, left - node.Diameter / 2);
+            whiteboard.Children.Add(node);
+        }
+
+        private void whiteboard_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (nodeToggleBtn.IsChecked == true)
+            {
+                Point position = e.GetPosition(whiteboard);
+                addNode(position.Y, position.X);
+            }
+        }
+
+        private void node_RemoveTriggered(object sender, EventArgs e)
+        {
+            whiteboard.Children.Remove((UIElement)sender);
         }
     }
 }
---
+++ GraphingApp/Node.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphingApp
+{
+    /// <summary>
+    /// Interaction logic for Node.xaml
+    /// </summary>
+    public partial class Node : UserControl
+    {
+        public event EventHandler RemoveTriggered;
+
+        #region Diameter property
+
+        public int Diameter
+        {
+            get { return (int)GetValue(DiameterProperty); }
+            set { SetValue(DiameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty DiameterProperty = 
+            DependencyProperty.Register("DiameterProperty", typeof(int), typeof(Node), new PropertyMetadata(25, OnDiameterPropertyChanged));
+
+        private static void OnDiameterPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as Node).Circle.Height = (int)e.NewValue;
+            (d as Node).Circle.Width = (int)e.NewValue;
+        }
+
+        #endregion
+
+        public Node()
+        {            
+            InitializeComponent();
+        }
+
+        private void Control_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RemoveTriggered(this, EventArgs.Empty);
+        }
+    }
+}
